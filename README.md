a04ba5789710d2f37d553f8a79f658c99752894e
mihee kim <jello0097@hanmail.net>
2024-07-16 15:27:49 -0400

    set up swagger

--- README.md
+++ README.md
@@ -1,40 +1,51 @@
-# Eco Picker API 
+# Eco Picker API
 
 ## Prerequisites
+
 - JDK 17 or higher
 - Gradle
 - IDE (IntelliJ IDEA recommended)
 
 ## Getting Started
 
-### git clone 
+### git clone
+
 ```shell
 git clone https://github.com/Eco-Picker/eco-picker-api.git
 cd eco-picker-api
 ```
+
 ### set an env file
+
 ```shell
 cp .env.exmaple .env
 ```
 
-### bootRun 
+### bootRun
+
 ```shell
 ./gradlew bootRun
 ```
 
+### Swagger
+
+http://localhost:15000/swagger-ui/index.html
+
 ## License
+
 This project is licensed under the MIT License - see the LICENSE file for details.
 
 Contact
 For any questions or feedback, please contact
+
 - [jerry2219398\@gmail.com](mailto:jerry2219398@gmail.com?subject=ecopicker)
 - [jsunwoo0977\@gmail.com](mailto:jsunwoo0977@gmail.com?subject=ecopicker)
 
-### TODO 
-- [ ] set up swagger
+### TODO
+
 - [ ] convention
-- [ ] set up db 
+- [ ] set up db
 - [ ] set up logging
-- [ ] set up spring security 
+- [ ] set up spring security
 - [ ] images - s3, local, etc
 - [ ] locations - map 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,60 @@
 plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("plugin.jpa") version "1.9.24"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("plugin.jpa") version "1.9.24"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "com.eco-picker"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 configurations {
-	all {
-		exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
-		exclude(group = "commons-logging", module = "commons-logging")
-	}
+    all {
+        exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-mustache")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("io.github.oshai:kotlin-logging:6.0.3")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // spring boot
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // logging
+    implementation("io.github.oshai:kotlin-logging:6.0.3")
+    // open API
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.3")
+    // database
+    runtimeOnly("com.h2database:h2")
+    // dev
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
